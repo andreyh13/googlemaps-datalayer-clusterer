7b53fe9ebe80035e05974a5ce4004d20854f9d44
Andriy Khomenko <andriy.khomenko@intermodaltelematics.com>
2019-11-21 17:59:25 +0000

    iteration 20191121

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    }, ],
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
+  plugins: [new CopyWebpackPlugin([{ from: 'src/images', to: 'images' }])],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
